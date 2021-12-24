61d7c5d0964b8fdf9c26985c1957bc6cdfe33a05
now4ever <now4ever@naver.com>
2021-12-24 15:29:19 +0900

    1개 수정, 2개 추가 by chief

--- demo3/src/main/java/com/gr/herb/Demo3Application.java
+++ demo3/src/main/java/com/gr/herb/Demo3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo3Application {
 
 	public static void main(String[] args) {
+		System.out.println("hello~~ by chief~");
 		SpringApplication.run(Demo3Application.class, args);
 	}
 
